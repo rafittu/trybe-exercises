e2c82e41bcd0f68764ba73eafef8dea347c6eb08
Rafael <rafittu@outlook.com>
2021-02-28 17:39:07 -0300

    adicionado exercicio 7, bloco 4.1

--- exercises/bloco_4/dia_1/exercicio_5.js
+++ exercises/bloco_4/dia_1/exercicio_5.js
@@ -42,3 +42,30 @@ switch(peça.toLowerCase()) {
         console.log('peça inválida');
         break;
 }
+
+
+// Exercício 7
+
+let score = 81;
+
+if (score < 0 || score > 100){
+    console.log('valor inválido');
+}
+else if (score >= 90) {
+    console.log('A');
+}
+else if (score >= 80 && score < 90){
+    console.log('B');
+}
+else if (score >= 70 && score < 80){
+    console.log('C');
+}
+else if (score >= 60 && score < 70){
+    console.log('D');
+}
+else if (score >= 50 && score < 60){
+    console.log('E');
+}
+else {
+    console.log('F');
+}
