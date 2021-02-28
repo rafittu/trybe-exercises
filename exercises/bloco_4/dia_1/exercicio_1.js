1768ed016993ac1c5992f19c66b1f2e977adf53f
Rafael <rafittu@outlook.com>
2021-02-28 16:36:16 -0300

    adicionado exercicio 2, bloco 4.1

--- exercises/bloco_4/dia_1/exercicio_1.js
+++ exercises/bloco_4/dia_1/exercicio_1.js
@@ -1,3 +1,5 @@
+// Exercício 1
+
 let a = 7;
 let b = 3;
 
@@ -6,3 +8,13 @@ console.log('Subtração: ' + (a - b));
 console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
+
+
+// Exercício 2
+
+if (a > b){
+    console.log('"a" é maior que "b"');
+}
+else {
+    console.log('"a" é menor que "b"');
+}
