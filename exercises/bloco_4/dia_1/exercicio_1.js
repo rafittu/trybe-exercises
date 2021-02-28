80091a490aaea081089d01350a504b0ea5130e87
Rafael <rafittu@outlook.com>
2021-02-28 16:45:19 -0300

    adicionado exercicio 3, bloco 4.1

--- exercises/bloco_4/dia_1/exercicio_1.js
+++ exercises/bloco_4/dia_1/exercicio_1.js
@@ -18,3 +18,18 @@ if (a > b){
 else {
     console.log('"a" é menor que "b"');
 }
+
+
+// Exercício 3
+
+let c = 9;
+
+if (a > b && a > c){
+    console.log('o maior número é o "a"');
+}
+else if (b > a && b > c){
+    console.log('o maior número é o "b"');
+}
+else {
+    console.log('o maior número é o "c"');
+}
