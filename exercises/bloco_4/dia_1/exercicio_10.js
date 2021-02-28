604aeb671a79f5436657384f988396920f56b18b
Rafael <rafittu@outlook.com>
2021-02-28 18:55:18 -0300

    adicionado exercicio 11, bloco 4.1

--- exercises/bloco_4/dia_1/exercicio_10.js
+++ exercises/bloco_4/dia_1/exercicio_10.js
@@ -11,3 +11,43 @@ if (custo >= 0 && venda >=0){
 else {
     console.log('valor de entrada menor que zero');
 }
+
+
+// Exercício 11
+
+let salarioBruto = 27000;
+let INSS;
+let IR;
+
+if (salarioBruto <= 1556.94){
+    INSS = salarioBruto * 0.8;
+}
+else if (salarioBruto <= 2594.92){
+    INSS = salarioBruto * 0.9;
+}
+else if (salarioBruto <= 5189.82){
+    INSS = salarioBruto * 0.11;
+}
+else {
+    INSS = 570.88
+}
+
+let salarioLiquido = salarioBruto - INSS;
+
+if (salarioLiquido <= 1903.98){
+    IR = 0;
+}
+else if (salarioLiquido <= 2826.65){
+    IR = (salarioLiquido * 0.075) - 142.80;
+}
+else if (salarioLiquido <= 3751.05){
+    IR = (salarioLiquido * 0.15) - 354.80;
+}
+else if (salarioLiquido <= 4664.68){
+    IR = (salarioLiquido * 0.225) - 636.13;
+}
+else {
+    IR = (salarioLiquido * 0.275) - 869.36;
+}
+
+console.log('Salario: ' + (salarioLiquido - IR));
