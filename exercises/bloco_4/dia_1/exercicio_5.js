36af1cc469ffd784f52073ba3d8f8eba73c4a683
Rafael <rafittu@outlook.com>
2021-02-28 17:06:29 -0300

    adicionado exercicio 5, bloco 4.1

---
+++ exercises/bloco_4/dia_1/exercicio_5.js
@@ -0,0 +1,15 @@
+// Exercício 5
+
+let A = 60;
+let B = 30;
+let C = 90;
+
+if (A + B + C === 180){
+    console.log('true');
+}
+else if (A + B + C > 180){
+    console.log('false');
+}
+else if (A + B + C < 0){
+    console.log('inválido');
+}
