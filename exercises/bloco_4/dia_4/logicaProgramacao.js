f5631bf484fc7231bddb5121cde83991decaf03d
Rafael <rafittu@outlook.com>
2021-03-01 19:34:53 -0300

    adicionado exercicio 5, bloco 4.4

--- exercises/bloco_4/dia_4/logicaProgramacao.js
+++ exercises/bloco_4/dia_4/logicaProgramacao.js
@@ -1,9 +1,9 @@
 // Exercicio 1
 
 let info = {
-    personagem: "Margarida",
-    origem: "Pato Donald",
-    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
   console.log('Bem-vinda, ' + info.personagem);
@@ -26,4 +26,26 @@ let info = {
 
   for (let key in info){
       console.log(info[key]);
+  }
+
+
+  // Exercício 5
+
+  let info2 = {
+      personagem: 'Tio Patinhas',
+      origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+      nota: 'O último MacPatinhas',
+      recorrente: 'Sim'
+  }
+
+  for (let key2 in info){
+      if (
+          info[key2] === info.recorrente &&
+          info[key2] === 'Sim' &&
+          info2[key2] === 'Sim'
+      ) {
+          console.log('Ambos recorrentes');
+      } else {
+          console.log(info[key2] + ' e ' + info2[key2]);
+      }
   }
