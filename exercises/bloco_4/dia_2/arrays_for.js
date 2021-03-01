f3233e272c93eb19c7b2e9df7f6e9ec701a05220
Rafael <rafittu@outlook.com>
2021-02-28 22:15:21 -0300

    adicionado exercicio 2, bloco 4.2

--- exercises/bloco_4/dia_2/arrays_for.js
+++ exercises/bloco_4/dia_2/arrays_for.js
@@ -2,4 +2,16 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers);
+for (let valores = 0; valores < numbers.length; valores++) {
+  console.log(numbers[valores]);
+}
+
+
+// Exercício 2
+
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++){
+    soma += numbers[i];
+}
+console.log(soma);
