8a1f4b8eb0bb6b7712fa5399dcbbb31f19594a1e
Rafael <rafittu@outlook.com>
2021-02-28 22:55:13 -0300

    adicionado exercicios 8 e 9, bloco 4.2

---
+++ exercises/bloco_4/dia_2/exercicio_8.js
@@ -0,0 +1,16 @@
+// Exercício 8
+
+let numbers = [];
+
+for (let i = 1; i <= 25; i += 1){
+    numbers.push(i);
+}
+
+console.log(numbers);
+
+
+// Exercício 9
+
+for (let i = 0; i < numbers.length; i +=1){
+    console.log(numbers[i] / 2);
+}
