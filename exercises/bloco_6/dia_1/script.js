21b2f864001e302a56d355cf191f862a3d7db04c
Rafael Ribeiro <rafittu@outlook.com>
2021-03-16 11:38:46 -0300

    refatorado funcao que gera os estados

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -1,16 +1,13 @@
 let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
 function createEstados() {
-  let getEstados = document.querySelector('#estado');
-
+  
   for (let index = 0; index < estados.length; index += 1) {
     let estado = estados[index];
     let estadoOption = document.createElement('option');
 
-    if (estado === estados[index]) {
-      estadoOption.innerHTML = estado;
-      getEstados.appendChild(estadoOption);
-    }
+    estadoOption.innerHTML = estado;
+    document.querySelector('#estado').appendChild(estadoOption);
   }
 }
 createEstados();
