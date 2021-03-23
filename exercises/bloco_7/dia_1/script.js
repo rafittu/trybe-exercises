ada5f5a6870b6e1e47e83f2aff792c595a82d2c1
Rafael Ribeiro <rafittu@outlook.com>
2021-03-23 18:33:06 -0300

    finalizado exercicios 7.1 parte I

--- exercises/bloco_7/dia_1/script.js
+++ exercises/bloco_7/dia_1/script.js
@@ -11,3 +11,26 @@ function testingScope(escopo) {
 }
 
 testingScope(true);
+
+// Exercício 1
+
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+// Exercício 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a, b){return a - b});
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
