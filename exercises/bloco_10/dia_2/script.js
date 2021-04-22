bc94e770dd7079684d086d2e674e6c706597e8f0
Rafael Ribeiro <rafittu@outlook.com>
2021-04-22 18:12:39 -0300

    exercicios 10.2

---
+++ exercises/bloco_10/dia_2/script.js
@@ -0,0 +1,48 @@
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+};
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      };
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animal = Animals.find((animal) => animal.name === name);
+      if (animal) {
+        return resolve(animal);
+      };
+      return reject('Nenhum animal com esse nome!');
+    }, 100);
+  })
+);
+
+const getAnimal = (name) => {
+  return findAnimalByName(name)
+  .then(animal => animal);
+};
+
+
+module.exports = { uppercase, findUserById, getUserName, findAnimalByName, getAnimal };
